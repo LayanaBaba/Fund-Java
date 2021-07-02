52702c2560f2cdf790baba78cbd427e0811c923a
LayanaBaba <leana_baba@yahoo.com>
2021-07-02 22:40:40 +0300

    Finished Lab03

--- linter/app/src/test/java/linter/AppTest.java
+++ linter/app/src/test/java/linter/AppTest.java
@@ -4,11 +4,55 @@
 package linter;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
+    @Test
+    public void AnalyzingTest(){
+        int[][] arr = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String actual = App.Analyzing(arr);
+        String expected = (
+                "High: " + 72 + "Low: " + 51
+                        + "Never saw temperature: " + 63
+                        + "Never saw temperature: " + 67
+                        + "Never saw temperature: " + 68
+                        + "Never saw temperature: " + 69
+        );
+
+        assertEquals(expected,actual);
     }
+    @Test public void tallyTest(){
+        List<String> votes = new ArrayList<>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+        String expected = "Bush received the most votes!";
+        String actual =App.tally(votes);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test public void readFileTest(){
+        ArrayList<String> targets = new ArrayList<>();
+        String filePath = "src/test/resources/gate.js";
+        boolean containsTargets = App.readFile(filePath).containsAll(targets);
+        assertTrue(containsTargets);
+    }
+
 }
